fc4ba394427e316fb41ad4cd4db04cce08b2fcb7
gousemohiddin44 <78602748+gousemohiddin44@users.noreply.github.com>
2021-07-04 11:00:22 +0530

    Update app.js
    Added atlas DB

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://admin-gouse:Test321@cluster0.3dgzt.mongodb.net/userDB",{useNewUrlParser:true,useUnifiedTopology:true});
 mongoose.set('useCreateIndex', true);
 
 //modified schema from JS object to new mongoose schema object.
